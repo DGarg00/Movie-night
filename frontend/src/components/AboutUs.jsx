c43550a05b4cb47e0c5cf74abe48933c0bc260fe
Divya Garg <160125761+DGarg00@users.noreply.github.com>
2026-07-25 12:17:11 +0530

    Update AboutUs.jsx

--- frontend/src/components/AboutUs.jsx
+++ frontend/src/components/AboutUs.jsx
@@ -7,7 +7,7 @@ export default function AboutUs() {
       <div className="about-card about-featured">
         <img
           className="about-photo about-photo-featured"
-          src="PASTE_TEACHER_PHOTO_URL_HERE"
+          src="https://drive.google.com/uc?export=view&id=1-Rzj-eLmv84mOkA_SBXQHcFwssSg1iNo"
           alt="Teacher name"
         />
         <div className="about-info">
